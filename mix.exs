81c9aaf2beca24f9845bbee9f31ac5d0271ec198
Marcin Lazar <>
2019-02-04 14:04:28 +0100

    update httpoison

--- mix.exs
+++ mix.exs
@@ -2,14 +2,15 @@ defmodule Dolla.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :dolla,
-     version: "0.3.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     package: package(),
-     description: description()
+    [
+      app: :dolla,
+      version: "0.3.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package(),
+      description: description()
     ]
   end
 
@@ -17,8 +18,7 @@ defmodule Dolla.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpoison, :timex, :timex_poison],
-     mod: {Dolla, []}]
+    [applications: [:logger, :httpoison, :timex, :timex_poison], mod: {Dolla, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -31,20 +31,21 @@ defmodule Dolla.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpoison, "~> 0.9"},
-     {:bypass, "~> 0.5", only: [:test]},
-     {:poison, "~> 2.0 or ~> 3.1"},
-     {:timex, "~> 3.1"},
-     {:timex_poison, "~> 0.1"},
-     {:ex_doc, ">= 0.0.0", only: :dev}
+    [
+      {:httpoison, "~> 1.5.0"},
+      {:bypass, "~> 0.5", only: [:test]},
+      {:poison, "~> 2.0 or ~> 3.1"},
+      {:timex, "~> 3.1"},
+      {:timex_poison, "~> 0.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 
   defp description do
     """
-    Dolla provides a wrapper over Apple's app store receipt verification service in Elixir.
+     Dolla provides a wrapper over Apple's app store receipt verification service in Elixir.
 
-   """
+    """
   end
 
   defp package do
